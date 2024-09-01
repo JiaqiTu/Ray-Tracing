81654b49da875ccd5602ca9f9f130d80b3b713ce
JiaqiTu <tujiajiagm@gmail.com>
2024-09-01 11:18:15 -0400

    add interval class. use interval for ray_t parameter

--- hittable_list.h
+++ hittable_list.h
@@ -22,13 +22,16 @@ class hittable_list : public hittable {
         objects.push_back(object);
     }
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    // bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) 
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         hit_record temp_rec;
         bool hit_anything = false;
-        auto closest_so_far = ray_tmax;
+        // auto closest_so_far = ray_tmax;
+        auto closest_so_far = ray_t.max;
 
         for (const auto& object : objects) {
-            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+            // if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+            if (object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 rec = temp_rec;
@@ -40,3 +43,9 @@ class hittable_list : public hittable {
 };
 
 #endif
+
+
+//The hit function in the hittable_list class is responsible for determining whether a ray intersects with any of the objects in a list of hittable objects. 
+// ray& r: This is the ray that is being tested for intersections with objects in the list.
+// interval ray_t: This is an interval representing the range of t values along the ray. The t value is a parameter that moves the point P(t) along the ray, where P(t) = origin + t * direction. The interval specifies the range of t values within which intersections are considered valid.
+// hit_record& rec: This is a structure that stores information about the closest intersection point found so far, such as the point of intersection, the surface normal at that point, and the t value. If an intersection is found, this record is updated.
