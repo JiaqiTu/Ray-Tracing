864f5dfddd2daf68067b21b9a1d04988a6187744
JiaqiTu <tujiajiagm@gmail.com>
2024-09-03 15:41:24 -0400

    solve infinity problem

--- interval.h
+++ interval.h
@@ -9,7 +9,8 @@ class interval {
     double min, max;
     // static const double infinity;    //dont need this because we have included the limits header
 
-    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+    // interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+    interval() : min(+std::numeric_limits<double>::infinity()), max(-std::numeric_limits<double>::infinity()) {} // Default interval is empty
 
     interval(double min, double max) : min(min), max(max) {}
 
@@ -34,7 +35,10 @@ class interval {
     static const interval empty, universe;
 };
 
-const interval interval::empty    = interval(+infinity, -infinity);
-const interval interval::universe = interval(-infinity, +infinity);
+// const interval interval::empty    = interval(+infinity, -infinity);
+// const interval interval::universe = interval(-infinity, +infinity);
+
+const interval interval::empty    = interval(+std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity());
+const interval interval::universe = interval(-std::numeric_limits<double>::infinity(), +std::numeric_limits<double>::infinity());
 
 #endif
